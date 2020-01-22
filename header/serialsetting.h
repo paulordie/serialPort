f4bf7baf642a028fc52d7dd7469770c238778a89
paulo.correa <paulo.correa@indt.org.br>
2020-01-22 10:38:21 -0400

    inicio da classe

---
+++ header/serialsetting.h
@@ -0,0 +1,10 @@
+#ifndef SERIALSETTING_H
+#define SERIALSETTING_H
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+#endif //SERIALSETTING_H
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+#include "header/serialsetting.h"
+
+using namespace std;
+int main(){
+    cout << "test!!" << endl;
+}
